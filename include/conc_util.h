2409f2e1536ca1d1e2d6877e464cbabe212bb451
gabriel <ferre490@gmail.com>
2019-10-07 20:43:44 -0300

    gitignore e include
    Colocando o gitignore e a pasta include, com timer.h e conc_util.h.

---
+++ include/conc_util.h
@@ -0,0 +1,125 @@
+#ifndef CONC_UTIL
+#define CONC_UTIL
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* Malloc(size_t size) {
+    void* p = malloc(size);
+    if(!p) {
+        printf("Falha ao alocar memoria\n");
+        exit(0);
+    }
+    return p;
+}
+
+void* Calloc(size_t nmemb, size_t size) {
+    void* p = calloc(nmemb, size);
+    if(!p) {
+        printf("Falha ao alocar memoria\n");
+        exit(0);
+    }
+    return p;
+}
+
+int Pthread_create(pthread_t* thread, pthread_attr_t* attr, void*(*start_routine)(void*), void* arg) {
+    int result = pthread_create(thread, attr, start_routine, arg);
+    if(result) {
+        printf("Falha ao criar thread\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_join(pthread_t thread, void** retval) {
+    int result = pthread_join(thread, retval);
+    if(result) {
+        printf("Falha ao juntar thread\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr) {
+    int result = pthread_mutex_init(mutex, attr);
+    if(result) {
+        printf("Falha ao inicializar mutex\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_mutex_destroy(pthread_mutex_t *mutex) {
+    int result = pthread_mutex_destroy(mutex);
+    if(result) {
+        printf("Falha ao destruir mutex\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_mutex_lock(pthread_mutex_t *mutex) {
+    int result = pthread_mutex_lock(mutex);
+    if(result) {
+        printf("Falha ao usar pthread_mutex_lock\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_mutex_unlock(pthread_mutex_t *mutex) {
+    int result = pthread_mutex_unlock(mutex);
+    if(result) {
+        printf("Falha ao usar pthread_mutex_unlock\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_cond_init(pthread_cond_t *restrict cond, const pthread_condattr_t *restrict attr) {
+    int result = pthread_cond_init(cond, attr);
+    if(result) {
+        printf("Falha ao inicializar condition\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_cond_destroy(pthread_cond_t *cond) {
+    int result = pthread_cond_destroy(cond);
+    if(result) {
+        printf("Falha ao destruir condition\n");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_cond_wait(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex) {
+    int result = pthread_cond_wait(cond, mutex);
+    if(result) {
+        printf("Falha ao usar pthread_cond_wait");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_cond_signal(pthread_cond_t *cond) {
+    int result = pthread_cond_signal(cond);
+    if(result) {
+        printf("Falha ao usar pthread_cond_signal");
+        exit(0);
+    }
+    return result;
+}
+
+int Pthread_cond_broadcast(pthread_cond_t *cond) {
+    int result = pthread_cond_broadcast(cond);
+    if(result) {
+        printf("Falha ao usar pthread_cond_broadcast");
+        exit(0);
+    }
+    return result;
+}
+
+#endif
